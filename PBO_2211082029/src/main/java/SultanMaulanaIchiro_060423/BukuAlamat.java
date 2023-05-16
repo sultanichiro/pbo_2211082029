af1a1cc58ab8bda76c9d2bfaab6b276c8739dd6a
sultanichiro <127267253+sultanichiro@users.noreply.github.com>
2023-05-16 22:14:10 +0700

    Add files via upload

---
+++ PBO_2211082029/src/main/java/SultanMaulanaIchiro_060423/BukuAlamat.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package SultanMaulanaIchiro_060423;
+
+/**
+ *
+ * @author 
+ */
+public class BukuAlamat {
+   public static void main (String[] args){
+        BukuAlamatData bukualamat1 = new BukuAlamatData("Fayy","Solok",
+        "082321210404","fay21@gmail.com");
+        bukualamat1.print("");
+    }
+
+}
