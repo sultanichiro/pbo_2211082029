aa5be78e07ff9bd18085c8e0d2ae1b4f8e7b7777
sultanichiro <127267253+sultanichiro@users.noreply.github.com>
2023-05-17 09:47:56 +0700

    Add files via upload

---
+++ PBO_2211082029/src/main/java/com/mycompany/pbo_2211082029/PBO_2211082029.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pbo_2211082029;
+
+/**
+ *
+ * @author PC9
+ */
+public class PBO_2211082029 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
